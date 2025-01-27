f8907b580e8c2881257890393e576b762a8f44cc
Avichai Gal_Or <avichai504@gmail.com>
2025-01-27 08:59:43 +0200

    init Avichai

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-export default nextConfig;
+const nextConfig = {
+  serverExternalPackages: ['knex'],
+  productionBrowserSourceMaps: false,
+}
+
+export default nextConfig
