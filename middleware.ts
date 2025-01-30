6791a3e4e6b8186b1f96bd0b1f918c784387d30d
zvijude <zvijude@gmail.com>
2025-01-30 10:59:20 +0200

    dfgdfg

--- middleware.ts
+++ middleware.ts
@@ -3,11 +3,11 @@ import type { NextRequest } from 'next/server'
 export async function middleware(request: NextRequest) {
   const user = request.cookies.get('user')?.value
 
-  if (!user && !request.nextUrl.pathname.startsWith('/auth') && !request.nextUrl.pathname.startsWith('/register')) {
+  if (!user) {
     return Response.redirect(new URL('/auth', request.url))
   }
 }
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
+  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$|manifest\\.json|auth(?:/.*)?).*)', '/register'],
 }
--- sql/default_update.sql
+++ sql/default_update.sql
@@ -1,12 +1,16 @@
-ALTER TABLE "MainTask"
+-- ALTER TABLE "MainTask"
+-- ALTER COLUMN "updatedAt" SET DEFAULT now();
+
+-- ALTER TABLE "Qr"
+-- ALTER COLUMN "updatedAt" SET DEFAULT now();
+
+-- ALTER TABLE "Task"
+-- ALTER COLUMN "updatedAt" SET DEFAULT now();
+
+-- ALTER TABLE "Prob"
+-- ALTER COLUMN "updatedAt" SET DEFAULT now();
+
+ALTER TABLE "User"
 ALTER COLUMN "updatedAt" SET DEFAULT now();
 
-ALTER TABLE "Qr"
-ALTER COLUMN "updatedAt" SET DEFAULT now();
-
-ALTER TABLE "Task"
-ALTER COLUMN "updatedAt" SET DEFAULT now();
-
-ALTER TABLE "Prob"
-ALTER COLUMN "updatedAt" SET DEFAULT now();
 
