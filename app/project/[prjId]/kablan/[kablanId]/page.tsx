1880dde6b33d0c8b548a50645af98eb792d18e9e
Avichai Gal_Or <avichai504@gmail.com>
2025-03-10 16:52:17 +0200

    fix stats ui  kablan

--- app/project/[prjId]/kablan/[kablanId]/page.tsx
+++ app/project/[prjId]/kablan/[kablanId]/page.tsx
@@ -3,6 +3,7 @@ import KablanTbl from '@/components/kablan/ui/KablanTbl'
 import { isManager } from '@/db/types'
 import StatsUi from '@/ui/StatsUi'
 import Link from 'next/link'
+import { formatCurrency } from 'zvijude/funcs'
 
 export default async function Kablan({ params }) {
   let { prjId, kablanId } = await params
@@ -11,15 +12,20 @@ export default async function Kablan({ params }) {
   const kablan = await getKablan(prjId, kablanId)
   const curKablanStats = await getKablanStats(prjId, kablanId)
 
+  // Ensure total_price is first
+  const orderedStats = {
+    total_price: curKablanStats.total_price,
+    ...Object.fromEntries(Object.entries(curKablanStats).filter(([key]) => key !== 'total_price' && key !== 'kablan_id')),
+  }
+
   return (
     <>
-      {/* כפתור חזרה לכל הקבלנים */}
       {isManager(user.role) && <Link href={`/project/${prjId}/kablan`}>חזרה לכל הקבלנים</Link>}
 
       {/* StatsUi for single Kablan */}
       <div className='flex mb-8'>
-        {Object.entries(curKablanStats).map(([key, value]) => {
-          if (key === 'kablan_id') return null // Exclude ID & name
+        {Object.entries(orderedStats).map(([key, value]) => {
+          if (['total_price', 'price_tasks', 'price_bgt_reqs'].includes(key)) value = formatCurrency(value)
           return <StatsUi key={key} lbl={kablanDic[key] || key} stat={value as string | number} />
         })}
       </div>
