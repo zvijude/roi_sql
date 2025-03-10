ed546576fbff44923785d166ce3459e4b7cae453
Avichai Gal_Or <avichai504@gmail.com>
2025-03-10 16:35:32 +0200

    delete all data from db file

---
+++ sql/delete_all.sql
@@ -0,0 +1,13 @@
+SELECT tablename FROM pg_tables WHERE schemaname = 'public';
+
+
+DO $$ 
+DECLARE 
+    r RECORD;
+BEGIN
+    FOR r IN (SELECT tablename FROM pg_tables WHERE schemaname = 'public') LOOP
+        EXECUTE 'TRUNCATE TABLE ' || quote_ident(r.tablename) || ' RESTART IDENTITY CASCADE';
+    END LOOP;
+
+END $$;
+
